b06a58cf6c38f270506b284e3fc69a4a6f954854
vipulpun <pm.vipul@gmail.com>
2023-09-20 22:34:54 +0545

    Add project files.

---
+++ Auth_sys/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth_sys
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
